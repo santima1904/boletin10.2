4183e8aa6a05c394ecc912529ff1a4f02654865b
Santi <santima1904@gmail.com>
2021-04-15 14:34:36 +0200

    Ejercicios 1 y 3 bien hechos

--- src/ejercicio3/Main/Main.java
+++ src/ejercicio3/Main/Main.java
@@ -4,7 +4,7 @@ import ejercicio3.clases.ClienteImp;
 
 public class Main {
     public static void main(String[] args) {
-        ClienteImp cliente = new ClienteImp("Ernesto", "Camela", 48, 23, );
+
     }
 }
 
